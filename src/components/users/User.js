9f140137e2ec8e10fe93aac96b475e9a89e1d917
Ryan Soto <rsoto003@ucr.edu>
2020-05-08 20:12:16 -0700

    changed navbar color and added layout for user page

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class User extends Component {
     componentDidMount(){
         this.props.getUser(this.props.match.params.login)
     }
+    
+    static propTypes = {
+        loading: PropTypes.bool.isRequired,
+        user: PropTypes.object.isRequired,
+        getUser: PropTypes.func.isRequired
+    }
+
     render(){
         const { 
             name, 
@@ -18,23 +27,56 @@ class User extends Component {
             following, 
             public_repos, 
             public_gists, 
-            hireable 
+            hireable, 
+            company
     } = this.props.user;
+
+
     const { loading } = this.props
-        if( loading ){
-            return <Spinner />
-        } else {
-            return (
-                <div>
-                    <img src={avatar_url} className="round-img" style={{ width: '130px'}}/>
-                    <h3>{name}</h3>
-                    <h2>{location}</h2>
-                    <div>{hireable}</div>
-                    <h3>{bio}</h3>
-                    <a href={blog}>Personal Links</a>
-                </div>
-            )
-        }
+
+    if( loading ) return <Spinner />;
+    
+    return <Fragment>
+        <Link to="/" className="btn btn-light"> 
+            Back To Search
+        </Link>
+        Hireable: {''}
+        {hireable ? <i className="fas fa-check text-success"></i> : <i className="fas fa-times-circle text-danger"></i>}
+        <div className="card grid-2">
+            <div className="all-center">
+                <img src={avatar_url} className="round-img" style={{width: '150px'}}/>
+                <h1>{name}</h1>
+                <p>{location}</p>
+            </div>
+            <div className="">
+                { bio && <Fragment>
+                    <h3>Bio</h3>
+                    <p>{bio}</p>
+                </Fragment>}
+                <a href={html_url} className="btn btn-dark my-1">Visit GitHub Profile</a>
+                <ul>
+                    <li>{login && <Fragment>
+                        <strong>Username:</strong> {login}
+                    </Fragment>}</li>
+                    <li>{company && <Fragment>
+                        <strong>Company:</strong> {company}
+                    </Fragment>}</li>
+
+                    <li>{blog && <Fragment>
+                        <strong>Website:</strong> {blog}
+                    </Fragment>}</li>
+                </ul>
+            </div>
+        </div>
+        
+        <div className="card text-center">
+            <div className="badge badge-primary">Followers: {followers}</div>
+            <div className="badge badge-success">Following: {following}</div>
+            <div className="badge badge-danger">Public Repos: {public_repos}</div>
+            <div className="badge badge-dark">Public Gists: {public_gists}</div>
+        </div>
+    </Fragment>
+
     }
 }
 
