3163bc234c500e2d6b7fa2d35d41ac14649452c0
Ryan Soto <rsoto003@ucr.edu>
2020-05-15 23:05:39 -0700

    refactoring stateful class components into functional components with useState hook

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,45 +1,49 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends Component {
-    state = {
-        text: ''
-    };
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-        clearUsers: PropTypes.func.isRequired,
-        users: PropTypes.array.isRequired,
-        setAlert: PropTypes.func.isRequired
-    }
+const Search = ({ searchUsers, clearUsers, users, setAlert }) => {
+   const [ text, setText ] = useState('')
 
-    handleSearch = event => {
-        const { value, name } = event.target;
-        this.setState({
-            [name]: value
-        })
-    };
+    const handleSearch = event => setText(event.target.value); 
 
-    searchSubmit = event => {
+    const searchSubmit = event => {
         event.preventDefault();
-        if(this.state.text === ''){
-            this.props.setAlert('Please enter something', 'danger')
+
+        if(text === ''){
+            setAlert('Please enter something', 'danger')
         } else {
-            this.props.searchUsers(this.state.text);
-            this.setState({text: ''})
+            searchUsers(text);
+            setText('')
         }
     };
 
-    render(){
-        const { clearUsers, users } = this.props;
         const deleteBtn = users.length > 0 && <input type="submit" value="Clear" className="btn btn-danger btn-block" onClick={clearUsers}/>
+
         return (
-            <form className="form" onSubmit={this.searchSubmit}>
-                <input type="text" name="text" placeholder="Search Users..." value={this.state.text} onChange={this.handleSearch} autoComplete="off"/>
-                <input type="submit" value="Search" className={this.state.text !== '' ? "btn btn-primary btn-block" : "btn btn-secondary btn-block"}/>
+            <form className="form" onSubmit={searchSubmit}>
+                <input 
+                    type="text" 
+                    name="text" 
+                    placeholder="Search Users..." 
+                    value={text} 
+                    onChange={handleSearch} 
+                    autoComplete="off"
+                />
+                <input 
+                    type="submit" 
+                    value="Search" 
+                    className={text !== '' ? "btn btn-primary btn-block" : "btn btn-secondary btn-block"}        
+                />
                 {deleteBtn}
             </form>
         );
-    }
+}
+
+Search.propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    users: PropTypes.array.isRequired,
+    setAlert: PropTypes.func.isRequired
 }
 
 export default Search;
